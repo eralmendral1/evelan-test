1a15df30d7234af9fbdaf8697a16ff6f37378ba6
m1racl3 <eralmendral@gmail.com>
2023-07-23 08:54:08 +0800

    chore: code alignment

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Link from 'next/link'
 
 export default async function Home() {
     return <div className='p-32'>
-            <Link href="/users"><h1 className='text-center text-3xl font-bold underline text-blue-500'>Visit Users Page</h1></Link>
+        <Link href="/users"><h1 className='text-center text-3xl font-bold underline text-blue-500'>Visit Users Page</h1></Link>
     </div>
 }
